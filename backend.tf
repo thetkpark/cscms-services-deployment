baeb526edb69ff99a4a0235f63de0e4dbf09c31d
Sethanant Pipatpakorn <sethanant.p@icloud.com>
2021-07-21 14:14:41 +0700

    ADD init version of terraform

---
+++ backend.tf
@@ -0,0 +1,13 @@
+# The block below configures Terraform to use the 'remote' backend with Terraform Cloud.
+# For more information, see https://www.terraform.io/docs/backends/types/remote.html
+terraform {
+  backend "remote" {
+    organization = "cscms"
+
+    workspaces {
+      name = "cscms-services-deployment"
+    }
+  }
+
+  required_version = ">= 0.13.0"
+}
---
+++ main.tf
@@ -0,0 +1,40 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "2.68.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+data "azurerm_resource_group" "cscms_rg" {
+  name = "cscms.me"
+}
+
+resource "azurerm_kubernetes_cluster" "cscms-services" {
+  name                = "cscms-services"
+  location            = data.azurerm_resource_group.cscms_rg.location
+  resource_group_name = data.azurerm_resource_group.cscms_rg.name
+  dns_prefix = "cscms"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_B1s"
+    enable_auto_scaling = false
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
+
+output "kube_config" {
+  value = azurerm_kubernetes_cluster.cscms-services.kube_config_raw
+  sensitive = true
+}
