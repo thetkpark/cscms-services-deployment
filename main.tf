717dfdf2eaf10458aad1eaef733da78ac1cabb05
Sethanant Pipatpakorn <sethanant.p@icloud.com>
2021-07-21 15:26:46 +0700

    Update vm_size to B2s

--- main.tf
+++ main.tf
@@ -24,7 +24,7 @@ resource "azurerm_kubernetes_cluster" "cscms-services" {
   default_node_pool {
     name       = "default"
     node_count = 1
-    vm_size    = "Standard_B1s"
+    vm_size    = "Standard_B2s"
     enable_auto_scaling = false
   }
 
