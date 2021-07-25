aaab38fe1c19b28feb4443ef6c9173dd43954fd7
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2021-07-25 20:20:23 +0700

    FIX k8s config

--- main.tf
+++ main.tf
@@ -18,9 +18,9 @@ provider "azurerm" {
 provider "helm" {
   kubernetes {
     host = azurerm_kubernetes_cluster.cscms-services.kube_config.0.host
-    client_key = base64decode(azurerm_kubernetes_cluster.cscms-services.kube_config.0.client_certificate) 
-    client_certificate  =  base64decode(azurerm_kubernetes_cluster.cscms-services.kube_config.0.client_key)
-    cluster_ca_certificate  =  base64decode(azurerm_kubernetes_cluster.cscms-services.kube_config.0.client_key)
+    client_key = base64decode(azurerm_kubernetes_cluster.cscms-services.kube_config.0.client_key)
+    client_certificate  =  base64decode(azurerm_kubernetes_cluster.cscms-services.kube_config.0.client_certificate) 
+    cluster_ca_certificate  =  base64decode(azurerm_kubernetes_cluster.cscms-services.kube_config.0.cluster_ca_certificate)
   }
 }
 
