f5b3a49f7931e7d74c8c66bf23cc2d0a63938e9f
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2021-07-25 20:09:23 +0700

    ADD helm ingress_nginx

--- main.tf
+++ main.tf
@@ -4,11 +4,24 @@ terraform {
       source = "hashicorp/azurerm"
       version = "2.68.0"
     }
+    helm = {
+      source = "hashicorp/helm"
+      version = "2.2.0"
+    }
   }
 }
 
 provider "azurerm" {
   features {}
+}
+
+provider "helm" {
+  kubernetes {
+    host = azurerm_kubernetes_cluster.cscms-services.kube_config.0.host
+    client_key = base64decode(azurerm_kubernetes_cluster.cscms-services.kube_config.0.client_certificate) 
+    client_certificate  =  base64decode(azurerm_kubernetes_cluster.cscms-services.kube_config.0.client_key)
+    cluster_ca_certificate  =  base64decode(azurerm_kubernetes_cluster.cscms-services.kube_config.0.client_key)
+  }
 }
 
 data "azurerm_resource_group" "cscms_rg" {
@@ -31,4 +44,12 @@ resource "azurerm_kubernetes_cluster" "cscms-services" {
   identity {
     type = "SystemAssigned"
   }
+}
+
+resource "helm_release" "ingress_nginx" {
+  name = "ingress-nginx"
+  namespace = "ingress-nginx"
+  create_namespace = true
+  repository = "https://kubernetes.github.io/ingress-nginx"
+  chart = "ingress-nginx/ingress-nginx"
 }
