2ff6be404b4288fb795e841e7bc08a6d9246c353
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2021-12-30 11:31:19 +0700

    test download

--- k6/cscms-storage.js
+++ k6/cscms-storage.js
@@ -2,14 +2,14 @@ import http from "k6/http"
 import { sleep } from "k6"
 
 export let options = {
-	vus: 5,
-	duration: "10s"
+	vus: 1,
+	duration: "1s"
 }
 
 export default function () {
+	const start = Date.now()
 	const res = http.get("https://storage.cscms.me/wmr5hp")
-	check(res, {
-		"is status 200": (r) => r.status === 200
-	})
+	const end = Date.now()
+	console.info(`${end - start}ms`)
 	sleep(1)
 }
