dd62ffe6f465aef845ca3d499c985cdc88269b99
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2025-04-07 10:12:53 +0700

    fix secret

--- scripts/decrypt-secrets.sh
+++ scripts/decrypt-secrets.sh
@@ -12,5 +12,5 @@ pattern="\\(.*\\)\\.enc\\.yaml"
 for file in "${files[@]}"
 do
   filename=$(expr "$file" : "$pattern")
-  SOPS_AGE_KEY=$AGE_KEY sops --decrypt  $file > "$filename.test.yaml"
+  SOPS_AGE_KEY=$AGE_KEY sops --decrypt  $file > "$filename.secret.yaml"
 done
